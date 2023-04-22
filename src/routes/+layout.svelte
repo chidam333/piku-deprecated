f51df66cbe52b1240c9b31b88ef12641a77a775b
chidam <chidam3chain@gmail.com>
2023-04-23 00:48:51 +0530

    whitespace error

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
-<script lang="ts">
-  import { dev } from "$app/environment";
-  import { inject } from "@vercel/analytics";
-  inject({ mode: dev ? "development" : "production" });
+<script>
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+inject({ mode: dev ? 'development' : 'production' });
   import "../app.css";
 </script>
 
